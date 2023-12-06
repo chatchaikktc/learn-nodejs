1d9a2c0439cde6a7ea4c72adf47dcb89e6cbe590
Bowie <chatchai.k@ktc.co.th>
2023-12-06 14:29:44 +0700

    เพิ่ม paht url "register"

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("ดีจ้าาาาา");
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/register', function (req, res) {
+    res.send('register page')
+  })
+
+app.listen(3000)
